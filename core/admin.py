b41161c4f432ce82fb75bfdeb7c4e3701468a1ea
Ednilton Gomes da Silva <edniltongsilva@gmail.com>
2025-08-07 11:22:33 -0300

    v.2 agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')# Exibe os campos na lista de eventos
+
+    list_filter = ('titulo','usuario','data_evento','data_criacao')# Adiciona filtros no banco de dados, vc pode filtrar por título, usuario, etc.
+
+admin.site.register(Evento, EventoAdmin)# Registra o modelo Evento com a classe EventoAdmin no site de administração do Django
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')# nome que vai aparecer na agenda
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')# relaciona o evento com o usuário que o criou
+    
+    class Meta:
+        db_table = 'evento'# especifica o nome da tabela no banco de dados seja 'evento'
+
+    def __str__(self):
+        return self.titulo # Retorna o título do evento quando o objeto é convertido para string 
